04a336aa7755a70e0938d269e00d82c449272803
Kushal Joshi <kushal@joshi.com>
2022-05-01 17:26:59 +0100

    added file write and read to demo

--- src/server.rs
+++ src/server.rs
@@ -5,9 +5,10 @@ mod point_capnp;
 
 pub mod point_demo {
     use crate::server::point_capnp::point;
-    use capnp::serialize_packed;
+    use capnp::serialize;
+    use std::fs::File;
 
-    pub fn write_to_stream() -> ::capnp::Result<()> {
+    pub fn write_to_stream() -> std::io::Result<()> {
         let mut message = ::capnp::message::Builder::new_default();
 
         let mut demo_point = message.init_root::<point::Builder>();
@@ -15,6 +16,28 @@ pub mod point_demo {
         demo_point.set_x(5_f32);
         demo_point.set_y(10_f32);
 
-        serialize_packed::write_message(&mut ::std::io::stdout(), &message)
+        // This Result should be consumed properly in an actual app
+        let _ = serialize::write_message(&mut ::std::io::stdout(), &message);
+
+        // Save the point
+        {
+            let file = File::create("point.txt")?;
+            let _ = serialize::write_message(file, &message);
+        }
+
+        // Read the point from file
+        {
+            let point_file = File::open("point.txt")?;
+
+            // We want this to panic in our demo incase there is an issue
+            let point_reader =
+                serialize::read_message(point_file, ::capnp::message::ReaderOptions::new())
+                    .unwrap();
+
+            let demo_point: point::Reader = point_reader.get_root().unwrap();
+            println!("\n(x = {}, y = {})", demo_point.get_x(), demo_point.get_y());
+        }
+
+        Ok(())
     }
 }
